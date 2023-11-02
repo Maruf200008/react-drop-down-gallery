16b1c94c043e1e4fd79c55b5b938ca2ce5b99265
Mohammad Maruf <mohammadmarufgazi@gamil.com>
2023-11-02 22:03:09 +0600

    working on

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { DndContext, closestCenter } from "@dnd-kit/core";
+import { DndContext, DragOverlay, closestCenter } from "@dnd-kit/core";
 import {
   SortableContext,
   arrayMove,
@@ -6,6 +6,7 @@ import {
 } from "@dnd-kit/sortable";
 
 import { useState } from "react";
+import { createPortal } from "react-dom";
 import {
   img1,
   img10,
@@ -25,6 +26,7 @@ function App() {
   const [count, setCount] = useState(0);
   const [selectId, setSelectId] = useState({});
   const [removeId, setRemoveId] = useState(new Set());
+  const [activeColumn, setActiveColumn] = useState(null);
   const [images, setImages] = useState([
     {
       id: 1,
@@ -119,16 +121,26 @@ function App() {
     });
   };
 
+  const onDragStart = (event) => {
+    console.log("DRAG START", event);
+    if (event.active.data.current.type === "Images") {
+      setActiveColumn(event.active.data.current.image);
+      return;
+    }
+  };
+
   return (
     <>
       <div className="max-w-screen-xl mx-auto p-4">
-        <div className=" my-10 flex items-center justify-between border-b pb-5">
+        <div className=" my-10 flex flex-col sm:flex-row items-center gap-5 justify-between border-b pb-5">
           <div>
             {/* check show count or not */}
             {count > 0 ? (
               <div className=" flex items-center gap-5">
                 <input type="checkbox" name="count" checked />
-                <p className=" text-xl font-semibold">{count} Files Selected</p>
+                <p className=" text-xl font-semibold">
+                  {count} {count === 1 ? "File" : "Files"} Selected
+                </p>
               </div>
             ) : (
               <h1 className=" font-semibold text-xl">Gallary</h1>
@@ -145,6 +157,7 @@ function App() {
         </div>
 
         <DndContext
+          onDragStart={onDragStart}
           collisionDetection={closestCenter}
           onDragEnd={handleDragEnd}
         >
@@ -152,17 +165,25 @@ function App() {
             items={images}
             strategy={verticalListSortingStrategy}
           >
-            <div className=" grid grid-cols-5 gap-10">
+            <div className=" grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5 sm:gap-7">
               {images.map((image, index) => (
                 <div
                   key={image?.id}
-                  className={index === 0 ? "col-span-2 row-span-2" : ""}
+                  className={
+                    index === 0 ? "col-span-2 md:row-span-2" : " col-span-1"
+                  }
                 >
                   <SortableItem image={image} handleCount={handleCount} />
                 </div>
               ))}
             </div>
           </SortableContext>
+          {createPortal(
+            <DragOverlay>
+              {activeColumn && <SortableItem image={activeColumn} />}
+            </DragOverlay>,
+            document.body
+          )}
         </DndContext>
       </div>
     </>
--- src/components/SortableItem.jsx
+++ src/components/SortableItem.jsx
@@ -6,27 +6,41 @@ import { BsImage } from "react-icons/bs";
 export default function SortableItem({ image, handleCount }) {
   const [select, setSelect] = useState(false);
 
-  const { attributes, listeners, setNodeRef, transform, transition } =
-    useSortable({ id: image?.id });
-  const style = { transform: CSS.Transform.toString(transform), transition };
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: image?.id,
+    data: {
+      type: "Images",
+      image,
+    },
+  });
+  const style = { transition, transform: CSS.Transform.toString(transform) };
 
   const handleCheck = (value) => {
     setSelect(!select);
     handleCount({ value, select });
   };
 
+  if (isDragging) {
+    return <div>Hellow</div>;
+  }
+
   return (
-    <div className="relative h-full group bg-slate-200 ">
+    <div ref={setNodeRef} style={style} className="relative h-full group">
       <div
-        ref={setNodeRef}
-        style={style}
         {...attributes}
         {...listeners}
         // check border style
         className={
           image?.img === null
-            ? "border border-dashed bg-neutral-100 border-neutral-600 rounded-md overflow-hidden relative  h-[100%] flex flex-col items-center justify-center"
-            : "border rounded-md overflow-hidden relative group"
+            ? "border-2 border-dashed bg-neutral-100 border-neutral-300 rounded-md overflow-hidden relative  h-[100%] flex flex-col items-center justify-center"
+            : "border-2 border-neutral-300 rounded-md overflow-hidden relative group"
         }
       >
         <div>
@@ -37,34 +51,40 @@ export default function SortableItem({ image, handleCount }) {
                 <div className=" text-[20px]">
                   <BsImage />
                 </div>
-                <div className=" text-[20px]">
+                <div className=" sm:text-[20px]">
                   <p> Add Images</p>
                 </div>
               </div>
-              <div className=" bg-transparent group-hover:bg-black/20 ease-in duration-300  h-full w-full absolute top-0 left-0" />
+              <div className=" bg-transparent group-hover:bg-black/30 ease-in duration-300  h-full w-full absolute top-0 left-0" />
             </div>
           ) : (
-            <div className=" ">
+            <div>
               <img src={image?.img} alt="image" className="img-container" />
-              <div className=" bg-transparent group-hover:bg-black/20 ease-in duration-300  h-full w-full absolute top-0 left-0" />
+              <div
+                className={`bg-transparent ${
+                  !select ? "group-hover:bg-black/40" : " bg-white/50"
+                } ease-in duration-300  h-full w-full absolute top-0 left-0`}
+              />
             </div>
           )}
         </div>
       </div>
-      <div
-        className={
-          select
-            ? " absolute left-5 top-5"
-            : " absolute left-5 top-5 hidden group-hover:block"
-        }
-      >
-        <input
-          type="checkbox"
-          name="check"
-          checked={image.selected}
-          onChange={() => handleCheck(image.id)}
-        />
-      </div>
+      {image?.img !== null ? (
+        <div
+          className={
+            select
+              ? " absolute left-3 sm:left-5 top-3 sm:top-5"
+              : " absolute left-3 top-3 sm:left-5 sm:top-5 hidden group-hover:block"
+          }
+        >
+          <input
+            type="checkbox"
+            name="check"
+            checked={image?.selected}
+            onChange={() => handleCheck(image?.id)}
+          />
+        </div>
+      ) : null}
     </div>
   );
 }
